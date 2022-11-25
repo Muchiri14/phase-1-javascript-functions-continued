16d0c8edd7d07f36ec106518c97685464ac6a734
Daniel <muchiridaniel59@gmail.com>
2022-11-24 22:57:51 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+function saturdayFun(activity = 'roller-skate'){
+    return `This Saturday, I want to ${activity}!`
+}
+let mondayWork = function (work = 'go to the office'){
+    return `This Monday, I will ${work}.`
+}
+function wrapAdjective (adjective){
+    return function (key = "special"){
+        return `You are ${adjective}${key}${adjective}!`
+    }
+
+}
